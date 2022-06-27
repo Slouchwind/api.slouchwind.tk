a7bf786af0a11dc70c32ff934c92974477dc9f11
Slouchwind <2241051890@qq.com>
2022-06-27 23:14:19 +0800

    t

--- api/test.js
+++ api/test.js
@@ -2,5 +2,5 @@ const fs = require("fs");
 
 export default function handler(req, res) {
     const { name } = req.query;
-    res.status(200).send(fs.readFileSync("../293a4339059e60b5_1_post.jpeg"));
+    res.status(200).send(fs.readFileSync("../293a4339059e60b5_1_post.jpeg").toString());
 }
