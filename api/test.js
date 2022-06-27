65d72ed79a3fe04ffa8ab1ae37e0a22d965442a7
Slouchwind <2241051890@qq.com>
2022-06-27 21:48:41 +0800

    t

--- api/test.js
+++ api/test.js
@@ -1,4 +1,6 @@
 export default function handler(request, response) {
     const { name } = request.query;
     response.status(200).send(`Hello ${name}!`);
+    response.status(200).send("\n");
+    response.status(200).send(request.query);
 }
