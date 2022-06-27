87ecf32ebfe1abdd8e8b38a2c852b3549e64dd76
Slouchwind <2241051890@qq.com>
2022-06-27 23:28:53 +0800

    t

--- api/test.js
+++ api/test.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 module.exports = (req, res) => {
     const { name } = req.query;
-    fs.writeFileSync("5.svg", `<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+    fs.writeFileSync("../5.svg", `<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
     <text x="0" y="15" fill="red">I love SVG</text>
   </svg>`);
     res.status(200).send("5");
