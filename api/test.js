8141ca59d9ef354e700adf84a9d6761cb25a893a
Slouchwind <2241051890@qq.com>
2022-06-27 23:00:24 +0800

    t

--- api/test.js
+++ api/test.js
@@ -1,4 +1,4 @@
-export default function handler(request, response) {
-    const { name } = request.query;
-    response.status(200).send(request.query);
+export default function handler(req, res) {
+    const { name } = req.query;
+    res.status(200).send("<img src=\"https://static-qn.drblack-system.com/dbs-art/2021/02/293a4339059e60b5_1_post.jpeg?imageMogr2/gravity/Center/thumbnail/570x\">");
 }
