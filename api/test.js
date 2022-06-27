450379566b290e457b20dab0d9fcabeccf480e89
Slouchwind <2241051890@qq.com>
2022-06-27 23:09:50 +0800

    t

--- api/test.js
+++ api/test.js
@@ -1,4 +1,6 @@
+const fs = require("fs");
+
 export default function handler(req, res) {
     const { name } = req.query;
-    res.status(200).send('<svg xmlns="http:\/\/www.w3.org/2000/svg" version="1.1" height="24.0" width="605"><text x="0" y="20" fill="#FF0000" font-size="20">你指尖跃动的电光，是我此生不变的信仰，唯我超电磁炮永世长存。</text></svg>');
+    res.status(200).send(fs.readFileSync("../293a4339059e60b5_1_post.jpeg"));
 }
