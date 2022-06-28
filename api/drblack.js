1c70710f0b7f9f22dc09561985a3bbec48390366
Slouchwind <2241051890@qq.com>
2022-06-28 16:47:06 +0800

    dr

--- api/drblack.js
+++ api/drblack.js
@@ -1,6 +1,7 @@
 module.exports = (req, res) => {
     var { time, date } = req.query;
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type", "image");
     res.send("<img src=\"https:\/\/static-qn.drblack-system.com/dbs-art/2021/05/1438918017c4b0f608_1_cover.jpg?imageMogr2/gravity/Center/thumbnail/570x\">");
-    return res.setHeader("Content-Type", "image");
+    return;
 };
