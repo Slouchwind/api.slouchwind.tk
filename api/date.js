c005b93197f47938b75c62e81eaad84d9459768b
Slouchwind <2241051890@qq.com>
2022-06-28 20:41:12 +0800

    d

--- api/date.js
+++ api/date.js
@@ -1,22 +1,24 @@
 var getTime = {
-    time: function (dt) {
-        let date = new Date(dt || null);
-        let h = date.getHours();
-        if (h < 10) h = "0" + h;
-        let m = date.getMinutes();
-        if (m < 10) m = "0" + m;
-        let s = date.getSeconds();
-        if (s < 10) s = "0" + s;
-        return h + ":" + m + ":" + s;
-    },
-    date: function (dt) {
-        let date = new Date(dt || null);
-        let y = date.getFullYear();
-        let m = date.getMonth() + 1;
-        if (m < 10) m = "0" + m;
-        let d = date.getDate();
-        if (d < 10) d = "0" + d;
-        return y + "年" + m + "月" + d + "日";
+    new: {
+        time: function () {
+            let date = new Date();
+            let h = date.getHours();
+            if (h < 10) h = "0" + h;
+            let m = date.getMinutes();
+            if (m < 10) m = "0" + m;
+            let s = date.getSeconds();
+            if (s < 10) s = "0" + s;
+            return h + ":" + m + ":" + s;
+        },
+        date: function () {
+            let date = new Date();
+            let y = date.getFullYear();
+            let m = date.getMonth() + 1;
+            if (m < 10) m = "0" + m;
+            let d = date.getDate();
+            if (d < 10) d = "0" + d;
+            return y + "年" + m + "月" + d + "日";
+        }
     }
 }
 
@@ -24,5 +26,5 @@ module.exports = (req, res) => {
     var { time, date } = req.query;
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "application/json");
-    return res.json({ date_text: `${getTime.date(date)} ${getTime.time(time)}` });
+    return res.json({ date_text: `${getTime.new.date()} ${getTime.new.time()}` });
 };
