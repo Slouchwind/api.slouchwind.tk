da6f7cbf5d67754cb0e5ea725dd1453cf85b33bb
Slouchwind <2241051890@qq.com>
2022-07-10 13:48:19 +0800

    ts

--- api/ts.ts
+++ api/ts.ts
@@ -1,7 +1,15 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (req: VercelRequest, res: VercelResponse) => {
-    const { name } = req.query;
-    res.send(`Hello ${name}`);
-    res.status(200).end();
+    var name;
+    if (req.query.name === undefined) {
+        name = req.body.name;
+    }
+    else {
+        name = req.query.name;
+    }
+    res.send(`Hello ${name}`+"\n")
+        .send(req.method)
+        .status(200)
+        .end();
 }
