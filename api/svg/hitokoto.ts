08533d06f41e47a9d3f93fb5af59e63f70a29c31
Slouchwind <2241051890@qq.com>
2023-01-03 09:56:54 +0800

    Fix bug

--- api/svg/hitokoto.ts
+++ api/svg/hitokoto.ts
@@ -5,11 +5,12 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     let reqC: string;
     if (c instanceof String) reqC = `c=${c}`;
     if (c instanceof Array) reqC = c.map(v => `c=${v}`).join('');
-    const fetchRes = await fetch(`https://v1.hitokoto.cn/?${c}`).then(r => r.json());
-    const { hitokoto } = fetchRes
-    response.status(200).send(`
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="${hitokoto.length * 20}px" height="20px">
-    <text x="0" y="15" fill="${fill}">${hitokoto}</text>
-</svg>
-`);
+    fetch(`https://v1.hitokoto.cn/?${c}`).then(r => r.json()).then(fetchRes => {
+        const { hitokoto } = fetchRes;
+        response.status(200).send(`
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="${hitokoto.length * 20}px" height="20px">
+            <text x="0" y="15" fill="${fill}">${hitokoto}</text>
+        </svg>
+        `);
+    });
 };
