caa61d6f74de232bc766507a089d437d9d461b73
Slouchwind <2241051890@qq.com>
2023-01-03 13:40:18 +0800

    Fix: fetch => axios

--- api/svg/hitokoto.ts
+++ api/svg/hitokoto.ts
@@ -1,15 +1,13 @@
-import fetch from 'node-fetch';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import axios from 'axios';
 
-interface hitokotoRes {
-    hitokoto: string;
-}
 export default async (request: VercelRequest, response: VercelResponse) => {
     const { fill = '#000', c = 'i' } = request.query;
     let reqC: string;
     if (c instanceof String) reqC = `c=${c}`;
     if (c instanceof Array) reqC = c.map(v => `c=${v}`).join('');
-    fetch(`https://v1.hitokoto.cn/?${c}`).then<hitokotoRes | Object>(r => r.json()).then((fetchRes: any) => {
+    axios(`https://v1.hitokoto.cn/?${c}`).then(r => {
+        const fetchRes = r.data;
         const { hitokoto } = fetchRes;
         response.status(200).send(`
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="${hitokoto.length * 20}px" height="20px">
