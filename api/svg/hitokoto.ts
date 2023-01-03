05b2633d468087d6e004e3da017598e00b6a808b
Slouchwind <2241051890@qq.com>
2023-01-03 09:47:04 +0800

    new

---
+++ api/svg/hitokoto.ts
@@ -0,0 +1,15 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+    const { fill = '#000', c = 'i' } = request.query;
+    let reqC: string;
+    if (c instanceof String) reqC = `c=${c}`;
+    if (c instanceof Array) reqC = c.map(v => `c=${v}`).join('');
+    const fetchRes = await fetch(`https://v1.hitokoto.cn/?${c}`).then(r => r.json());
+    const { hitokoto } = fetchRes
+    response.status(200).send(`
+<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="${hitokoto.length * 20}px" height="20px">
+    <text x="0" y="15" fill="${fill}">${hitokoto}</text>
+</svg>
+`);
+};
--- api/test.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-    const { who = "World" } = req.query;
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    return res.json({ text: `Hello ${who}!` });
-}
