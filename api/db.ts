0654de9d05f377ae8f8b0d826c0dabadaeb6783b
Slouchwind <2241051890@qq.com>
2023-01-04 08:05:48 +0800

    db

--- api/db.ts
+++ api/db.ts
@@ -3,7 +3,8 @@ import { MongoClient } from 'mongodb';
 
 const MONGODB_URL = `mongodb+srv://slouch_data_user:${process.env.MONGODB_PASSWORD}@cluster0.7jecg4x.mongodb.net/?retryWrites=true&w=majority`;
 export default async (req: VercelRequest, res: VercelResponse) => {
-    const client = new MongoClient(MONGODB_URL);
-    await client.connect();
-    res.status(200).send((await client.db('test').collection('abc').find().toArray()).join());
+    let client = new MongoClient(MONGODB_URL);
+    client = await client.connect();
+    console.log(client);
+    res.status(200).send('done');
 }
